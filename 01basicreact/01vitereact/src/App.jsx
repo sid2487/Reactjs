ab8d0527035019d14ff24fa78e0a08c75557daeb
sid2487 <sid454804@gmail.com>
2025-04-21 14:48:59 +0530

    started react and learned basic and file structures

---
+++ 01basicreact/01vitereact/src/App.jsx
@@ -0,0 +1,14 @@
+import Chai from "./chai.jsx"
+
+function App() {
+
+  return (
+    <>
+    <Chai/>
+    <h1>Chai aur React</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae itaque, consectetur et fugit voluptatibus cum perferendis velit laudantium quisquam aspernatur molestiae sunt dignissimos quo vel, non quis officiis? Quis, doloremque!</p>
+    </>
+  )
+}
+
+export default App
---
+++ 01basicreact/01vitereact/src/main.jsx
@@ -0,0 +1,12 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+
+
+import Chai from './chai.jsx'
+import App from './App.jsx'
+
+createRoot(document.getElementById('root')).render(
+ 
+    <App />
+  
+)
