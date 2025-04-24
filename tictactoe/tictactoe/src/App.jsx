a76c1c55f01adcb583005d2a0be82a04d1b02f84
sid2487 <sid454804@gmail.com>
2025-04-24 08:02:40 +0530

    learnt about hooks and created a tictactoe game

---
+++ tictactoe/tictactoe/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+import Board from './TicTacToe/Board'
+
+function App() {
+  
+
+  return (
+    <>
+      <Board />
+    </>
+  );
+}
+
+export default App
